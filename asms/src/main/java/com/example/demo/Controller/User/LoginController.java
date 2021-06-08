8fa1176e2813f093d9fc45ed22f0613dfb6295ad
高野淇 <18351040@buaa.edu.cn>
2021-06-08 23:57:07 +0800

    1

--- asms/src/main/java/com/example/demo/Controller/User/LoginController.java
+++ asms/src/main/java/com/example/demo/Controller/User/LoginController.java
@@ -1,6 +1,7 @@
 package com.example.demo.Controller.User;
 import com.example.demo.Service.User.Impl.*;
 import com.example.demo.Service.User.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import com.example.demo.Entity.User.User;
 
@@ -9,19 +10,22 @@ import java.util.Map;
 
 @RestController
 public class LoginController {
+    @Autowired
     private UserService userService;
     @RequestMapping(value = "/admin/Login", method = RequestMethod.POST)
     public Map<String, Object> login(@RequestBody User user) {
         Map<String, Object> map = new HashMap<>();
         String password=user.getPassword();
         String username=user.getUsername();
-        if (user.getUsername().equals("admin") && user.getPassword().equals("123456")) {
-            map.put("message", "密码错误");
-            return map;
+        try{
+        User user1=userService.selectUserByUsername(username);
+        if(user)
         }
-        else {
-            map.put("message", "密码错误");
-            return map;
-        }
+        catch (Exception e) {
+        e.printStackTrace();
+        map.put("success", false);
+        map.put("message", "用户注册失败！");
+    }
+    return map;
     }
 }
